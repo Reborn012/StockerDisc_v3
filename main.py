4792ef2d8a980fe934ad1741fdca48ad6484653f
Rudrashis Mandal <165978738+Reborn012@users.noreply.github.com>
2025-07-20 14:25:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -43,6 +43,8 @@ async def on_message(message):
 @bot.command()
 async def hello(ctx):
     await ctx.send(f"Hello {ctx.author.mention}!")
+async def Kennedy(ctx):
+    await ctx.send(f"Hello Kennedy! how are you doing, hope you having a great day!")
 
 @bot.command()
 async def assign(ctx):
